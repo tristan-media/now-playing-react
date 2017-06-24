7177a7b83e32b651ac1499dac28bddace0ec56e1
Barry Hoggard <barry@tristanmedia.com>
2017-06-24 16:38:41 -0400

    put back counterstream

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Now Playing</h1>
-        {/*<Piece source="counterstream" name="Counterstream Radio" url="http://counterstreamradio.net/" />*/}
+        <Piece source="counterstream" name="Counterstream Radio" url="http://counterstreamradio.net/" />
         <Piece source="dronezone" name="Dronze Zone" url="http://somafm.com/dronezone/" />
         <Piece source="earwaves" name="Earwaves" url="http://somafm.com/earwaves/" />
         <Piece source="q2" name="Q2 Music" url="http://q2music.org/" />
