30ac5f7dcf5ac96f1cbbd17c6b806567457f0ac4
Barry Hoggard <barry@tristanmedia.com>
2018-01-03 05:54:47 -0500

    update Q2 to New Sounds

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <Piece source="counterstream" name="Counterstream Radio" url="http://counterstreamradio.net/" />
         <Piece source="dronezone" name="Dronze Zone" url="http://somafm.com/dronezone/" />
         <Piece source="earwaves" name="Earwaves" url="http://somafm.com/earwaves/" />
-        <Piece source="q2" name="Q2 Music" url="http://q2music.org/" />
+        <Piece source="q2" name="New Sounds" url="https://www.newsounds.org/" />
         <Piece source="yle" name="Yle Klassinen" url="http://yle.fi/radio/yleklassinen/suora/" />
       </div>
     );
